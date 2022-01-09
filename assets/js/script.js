87a793c1e3b8fc951173e3eca483193ee8e1d68b
unknown <williambrooks287@gmail.com>
2022-01-09 16:06:34 -0500

    add ability to save timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var indexId;
 
 // runs at beginning of app
 currentDate();
+startDay(makeArray());
+saveBtn();
 
 // display current time
 function currentDate() {
@@ -14,3 +16,30 @@ function currentDate() {
     console.log(date);
 };
 
+// create array to hold timeblocks
+function makeArray() {
+    var array = [];
+    array = $('textarea').map((i, element) => {
+        return $(element);
+    });
+    console.log(array);
+    return array;
+};
+
+// populate timeblocks from local storage
+function startDay(arr) {
+    arr.each(function (i) {
+      arr[i] = JSON.parse(localStorage.getItem(i));
+      this.val(arr[i]);
+      this.css('color', 'black');
+    });
+  }
+
+// save input to local storage
+function saveBtn() {
+    $('.saveBtn').on('click', function () {
+      userInput = $(this).siblings('.form-control').val().trim();
+      indexId = $(this).attr('data-id');
+      localStorage.setItem(indexId, JSON.stringify(userInput));
+    });
+  };
